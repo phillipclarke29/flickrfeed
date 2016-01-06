8b2ec89504f5001b89f908041223ea75794c881a
philip <philip.clarke@redhood.co.uk>
2016-01-06 12:32:22 +0000

    playing with progress bar

--- app.js
+++ app.js
@@ -4,5 +4,27 @@
 angular.module('flickrFeed', ['ngMaterial'])
   .controller('ListController', ['$scope', '$http', function($scope, $http){
 
+    $scope.results = [];
+
+    $scope.search = function(){
+
+      $http({
+        method: 'GET',
+        url: 'https://api.flickr.com/services/rest',
+        params: {
+          method: 'flickr.photos.search',
+          api_key: '0d9a81629077f3ee2cdefcdc91d09826',
+          text: $scope.searchTerm,
+          format: 'json',
+          nojsoncallback: 1
+        }
+      }).success(function (data){
+        $scope.results = data;
+          console.log(data);
+      }).error(function(error) {
+        console.log(error);
+      });
+
+    };
   }]);
 })();

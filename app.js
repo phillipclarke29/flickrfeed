522d9e566f8faa76bdd260466198b07741847522
philip <philip.clarke@redhood.co.uk>
2016-01-07 21:07:13 +0000

    returning an aoi object

--- app.js
+++ app.js
@@ -10,22 +10,10 @@ angular.module('flickrFeed', ['ngMaterial'])
 
     $scope.search = function(){
 
-
-      $http({
-        method: 'GET',
-        url: 'https://api.flickr.com/services/rest',
-        params: {
-          method: 'flickr.photos.search',
-          api_key: '0d9a81629077f3ee2cdefcdc91d09826',
-          text: $scope.searchTerm,
-          format: 'json',
-          nojsoncallback: 1
-        }
-      }).success(function (data){
-        $scope.results = data;
-          console.log(data);
-      }).error(function(error) {
-        console.log(error);
+    
+      $http.jsonp('https://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=JSON_CALLBACK&tags=').success(function(data){
+        console.log(data.items)
+        $scope.results = data.items;
       });
 
     };

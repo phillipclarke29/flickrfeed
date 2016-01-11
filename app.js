75d70903aaf8c78e19cdcefdd0535b0ed2b5c7dd
philip <philip.clarke@redhood.co.uk>
2016-01-11 10:41:52 +0000

    removed search circle

--- app.js
+++ app.js
@@ -6,15 +6,20 @@ angular.module('flickrFeed', ['ngMaterial'])
 
     $scope.results = [];
 
-    $scope.isSearching= false;
+      $scope.isSearching= false;
 
     $scope.search = function(){
+
+      $scope.isSearching= true;
+
       var term = $scope.searchTerm.split(" ")
       var url = 'https://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=JSON_CALLBACK' + '&tags="potato,"' + term
 
       $http.jsonp(url).success(function(data){
 
         $scope.results = data.items;
+
+        $scope.isSearching= false;
       });
 
     };

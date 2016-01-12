89426cfc9c096fa53d71c99df2718f5736757c30
philip <philip.clarke@redhood.co.uk>
2016-01-12 12:30:52 +0000

    resolved conlict

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-(function(){
-'use strict';
+var app = angular.module('flickrFeed', ['ngMaterial'])
 
-angular.module('flickrFeed', ['ngMaterial', 'angularModalService'])
-  .controller('ListController', ['$scope', '$http', function($scope, $http){
+app.controller('ListController', ['$scope', '$http', function($scope, $http){
 
     $scope.results = [];
 
@@ -24,4 +22,36 @@ angular.module('flickrFeed', ['ngMaterial', 'angularModalService'])
 
     };
   }]);
-})();
+  app.factory('verifyDelete', function($mdDialog) {
+    return function(user) {
+      var confirm = $mdDialog.confirm()
+        .title('Confirm Your Choice')
+        .content('Are you sure you want to delete ' + user.firstName + ' ' + user.lastName + '?')
+        .ariaLabel('Delete User')
+        .ok('Delete User')
+        .cancel('Cancel');
+      return $mdDialog.show(confirm);
+    }
+  })
+
+
+
+  app.controller('mainCtrl', function($scope, verifyDelete) {
+    $scope.users = [{
+      firstName: 'Obi-Wan',
+      lastName: 'Kenobi'
+    }, {
+      firstName: 'Boba',
+      lastName: 'Fett'
+    }, {
+      firstName: 'Han',
+      lastName: 'Solo'
+    }];
+
+    $scope.delete = function(user) {
+      verifyDelete(user).then(function() {
+        var index = $scope.users.indexOf(user);
+        $scope.users.splice(index, 1);
+      });
+    }
+  });

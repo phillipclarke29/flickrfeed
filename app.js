3acf2da2ee164e3ad1e354a345154fb506a93002
philip <philip.clarke@redhood.co.uk>
2016-01-12 13:02:44 +0000

    new modal service

--- app.js
+++ app.js
@@ -22,36 +22,3 @@ app.controller('ListController', ['$scope', '$http', function($scope, $http){
 
     };
   }]);
-  app.factory('verifyDelete', function($mdDialog) {
-    return function(user) {
-      var confirm = $mdDialog.confirm()
-        .title('Confirm Your Choice')
-        .content('Are you sure you want to delete ' + user.firstName + ' ' + user.lastName + '?')
-        .ariaLabel('Delete User')
-        .ok('Delete User')
-        .cancel('Cancel');
-      return $mdDialog.show(confirm);
-    }
-  })
-
-
-
-  app.controller('mainCtrl', function($scope, verifyDelete) {
-    $scope.users = [{
-      firstName: 'Obi-Wan',
-      lastName: 'Kenobi'
-    }, {
-      firstName: 'Boba',
-      lastName: 'Fett'
-    }, {
-      firstName: 'Han',
-      lastName: 'Solo'
-    }];
-
-    $scope.delete = function(user) {
-      verifyDelete(user).then(function() {
-        var index = $scope.users.indexOf(user);
-        $scope.users.splice(index, 1);
-      });
-    }
-  });
